ad137975fb13215b0ce9c823aba8a5b8cb5d201d
danielfpedro <danielfpedro@gmail.com>
2018-01-07 20:40:09 -0200

    coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core';
+
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    CoreModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clientes/clientes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientesListComponent } from './clientes-list/clientes-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ClientesListComponent]
+})
+export class ClientesModule { }
