e95cc8940fca19435f2651f1ae0a37e7e225f22c
danielfpedro <danielfpedro@gmail.com>
2018-01-07 23:00:41 -0200

    coisas

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-//Install express server
+// //Install express server
+// "use strict";
+// const express = require('express');
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
 "use strict";
-const express = require('express');
-const app = express();
+const express = require('express'),
+      app = express(),
+      morgan = require('morgan'),
+      path = require('path'),
+      portNumber = process.env.PORT || process.argv[2] || 8080;
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname)); 
+app.use(morgan('dev'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.listen(portNumber, function () {
+  console.log("Listening on port " + portNumber);
+});
