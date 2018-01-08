8cb0ee4fe62732ac793554393b9ec3726469b6be
danielfpedro <danielfpedro@gmail.com>
2018-01-07 23:06:13 -0200

    coisas

--- server.js
+++ server.js
@@ -8,22 +8,23 @@
 
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
-
-
 "use strict";
-const express = require('express'),
-      app = express(),
-      morgan = require('morgan'),
-      path = require('path'),
-      portNumber = process.env.PORT || process.argv[2] || 8080;
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-app.use(express.static(__dirname)); 
-app.use(morgan('dev'));
+// const api = require('./server/routes/api');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(portNumber, function () {
-  console.log("Listening on port " + portNumber);
-});
+const port = process.env.PORT || '3001';
+app.set('post', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running`));
